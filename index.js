09b9b5ed2e081424ca3f9772845f5b82433d5e14
Edward Hu <hues@usc.edu>
2016-07-01 20:48:07 -0700

    finished app

--- index.js
+++ index.js
@@ -5,24 +5,22 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import createLogger from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
+import configureStore from './store/configureStore';
+//import DevTools from './containers/DevTools';
 import reducer from './reducers/index.js';
 import App from './components/App.js';
 import { fetchDevices } from './actions';
 
-const loggerMiddleware = createLogger();
+const store = configureStore();
 
-const store = createStore(reducer,
-applyMiddleware(
-	thunkMiddleware,
-	loggerMiddleware
-	)
-);
+store.dispatch(fetchDevices())
 
-store.dispatch(fetchDevices()).then(() => console.log('state',store.getState()));
 
 render(
   <Provider store={store}>
-    <App />
+		<div>
+			<App />
+		</div>
   </Provider>,
   document.getElementById('root')
 );
