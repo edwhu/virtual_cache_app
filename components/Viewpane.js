4e45e30d1ba29fbc9934d28e6267388a9bd2d43f
Edward Hu <hues@usc.edu>
2016-07-08 11:29:48 -0700

    added new fields for table

--- components/Viewpane.js
+++ components/Viewpane.js
@@ -9,18 +9,16 @@ const Viewpane = ({selectedDevice}) => {
 
 	return (
 		<div style = {ViewpaneStyle}>
-			<table style={TableStyle}>
+			<table style={Object.assign({},TableStyle,{width:'40%'})}>
 				<tbody>
 					<tr>
 						<th style={TableDataStyle}>Name</th>
 						<th style={TableDataStyle}>location</th>
-						<th style={TableDataStyle}>d2d</th>
 						<th style={TableDataStyle}>date</th>
 					</tr>
 					<tr>
 						<td style={TableDataStyle}>{selectedDevice.name}</td>
 						<td style={TableDataStyle}>{selectedDevice.loc.join(' ')}</td>
-						<td style={TableDataStyle}>{selectedDevice.d2d}</td>
 						<td style={TableDataStyle}>{selectedDevice.time}</td>
 					</tr>
 				</tbody>
@@ -31,6 +29,9 @@ const Viewpane = ({selectedDevice}) => {
 						<th style={TableDataStyle}>Video</th>
 						<th style={TableDataStyle}>Duration</th>
 						<th style={TableDataStyle}>Length</th>
+						<th style={TableDataStyle}>Data</th>
+						<th style={TableDataStyle}>Wifi</th>
+						<th style={TableDataStyle}>Battery</th>
 					</tr>
 					{cacheArray}
 				</tbody>
