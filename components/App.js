ea188cc725881df46f129a40af071d950be71e02
Edward Hu <hues@usc.edu>
2016-07-14 00:11:07 -0700

    added selectednbdevicelist

--- components/App.js
+++ components/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import SelectedDeviceList from '../containers/SelectedDeviceList.js';
 import SelectedViewpane from '../containers/SelectedViewpane.js';
 import SelectedMap from '../containers/SelectedMap.js';
+import SelectedNearbyDeviceList from '../containers/SelectedNearbyDeviceList.js';
 const App = () => (
 	<div>
 		<SelectedMap />
 		<SelectedViewpane />
+		<SelectedNearbyDeviceList />
 		<SelectedDeviceList />
 	</div>
 );
