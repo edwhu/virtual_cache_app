2fa1ca7d76461698f4758cda125525a34913c7f2
Edward Hu <hues@usc.edu>
2016-07-18 14:46:13 -0700

    exportCSV

--- exportCSV.js
+++ exportCSV.js
@@ -1,39 +1,30 @@
-const MongoClient = require('mongodb').MongoClient;
-const MONGO_URL = require('./env.js').MONGO_URL;
 const Promise = require('bluebird');
 const fs = require('fs');
 const _ = require('lodash')
 
-const exportToCSV = array => {
-  const stream = fs.createWriteStream('file.csv');
-  const fields = ['name','date','loc','d2d','videoname', 'duration', 'length'];
-
-  //write the fields
-  stream.write(fields + '\n');
-  //begin writing values.
-  //console.log(array);
-  _.forEach(array, object => {
-    const name = object.name;
-    const loc = `"${object.loc}"`;
-    const d2d = `${object.d2d}`;
-    const date = `${object.date}`;
-    let cache = '';
-    _.forEach(object.cache, (video,i) => {
-      if(i === 0) cache +=`${video.name},${video.duration},${video.length}\n`
-      else cache+=`,,,,${video.name},${video.duration},${video.length}\n`;
-    })
-    stream.write(`${name},${date},${loc},${d2d}, ${cache}\n\n`);
+const exportToCSV = (array,filename) => {
+  return new Promise ( resolve => {
+    const stream = fs.createWriteStream(filename);
+    const fields = ['name','date','loc','d2d','videoname', 'duration', 'length'];
+    //write the fields
+    stream.write(fields + '\n');
+    //begin writing values.
+    //console.log(array);
+    _.forEach(array, object => {
+      const name = object.name;
+      const loc = `"${object.loc}"`;
+      const d2d = `${object.d2d}`;
+      const date = `${object.date}`;
+      let cache = '';
+      _.forEach(object.cache, (video,i) => {
+        if(i === 0) cache +=`${video.name},${video.duration},${video.length}\n`
+        else cache+=`,,,,${video.name},${video.duration},${video.length}\n`;
+      })
+      stream.write(`${name},${date},${loc},${d2d}, ${cache}\n\n`);
+    });
+    stream.end();
+    resolve(fs.createReadStream(filename));
   });
-  stream.end();
-  console.log('stream ended');
 }
 
-MongoClient.connect(MONGO_URL, (err, database) =>{
-  if(err) throw err;
-  const json = database
-    .collection('devices')
-    .find({},{_id:0})
-    .toArray();
-  json.then(exportToCSV).then( () => database.close());
-
-});
+module.exports = exportToCSV;
